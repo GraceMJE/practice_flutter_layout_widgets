43f65cd2d7fe5722d5d42ad74f5b37b8f13a45d3
GraceMJE <akwndms123@naver.com>
2023-10-08 21:28:28 +0900

    align widget으로 감싸기_근데 align끼리 쓰는게 복잡한 구현에서는 좋지 않음.

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,39 @@ class Body extends StatelessWidget {
             color: Colors.blue,
             fontSize: 32,
           ),
-        )
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.cyan,
+              borderRadius: BorderRadius.circular(150),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: 280,
+            height: 280,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(140),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Count 0',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 32,
+            ),
+          ),
+        ),
       ],
     );
   }
