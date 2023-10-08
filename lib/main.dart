c8f315cd0704396af52cbe3623943f3519c60280
GraceMJE <akwndms123@naver.com>
2023-10-08 21:25:56 +0900

    겹겹이 쌓은 container 간단구현

--- lib/main.dart
+++ lib/main.dart
@@ -17,24 +17,29 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack( //Stack은 container겹겹이 쌓을 수 있게 만듦
       children: [
-        Container(width: 500, height: 500, color: Colors.black),
-        Align(
-            alignment: Alignment.center,
-            // Alignment x,y 좌표도 가능
-            child: Container(
-                width: 400,
-                height: 400,
-                color: Colors.red)
+        Container(
+          width: 300,
+          height: 300,
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(150),
+          ),
         ),
-        Container(width: 300, height: 300, color: Colors.blue),
-        Positioned(
-            bottom: 50,
-            right: 30,
-            child: Container(
-                width: 200,
-                height: 200,
-                color: Colors.green)
+        Container(
+          width: 280,
+          height: 280,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(140),
+          ),
         ),
+        Text(
+          'Count 0',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 32,
+          ),
+        )
       ],
     );
   }
